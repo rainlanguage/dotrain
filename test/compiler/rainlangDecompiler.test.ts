aaa2232add9228850adeceb3c7ec83d414cfb568
SocioDroid <sociodroid2019@gmail.com>
2023-03-31 22:50:52 +0530

    Added error checks

--- test/compiler/rainlangDecompiler.test.ts
+++ test/compiler/rainlangDecompiler.test.ts
@@ -1,12 +1,12 @@
 import * as chai from "chai";
 import { rainlang } from "../../src/utils";
 import chaiAsPromised from 'chai-as-promised';
-import { deployerAddress } from "../utils";
+import { assertError, deployerAddress } from "../utils";
 import { rlc } from "../../src/compiler/rainCompiler";
 import { rld } from "../../src/compiler/rainDecompiler";
-import { getOpMetaFromSg } from "../../src";
-
-
+import { ExpressionConfig, getOpMetaFromSg } from "../../src";
+import { invalidOpMetas } from "../fixtures/opmeta";
+import { hexlify } from "ethers/lib/utils";
 chai.use(chaiAsPromised);
 const assert: Chai.AssertStatic = chai.assert;
 
@@ -17,31 +17,50 @@ describe("Rainlang Decompiler (rld) tests", async function () {
         opMeta = await getOpMetaFromSg(deployerAddress, "mumbai");
     });
 
+    it("should fail if an invalid bytes opmeta is specified", async () => {
+
+        const expression = rainlang`_: add(0x0a 0x14);`;
+        const expressionConfig = await rlc(expression, opMeta);
+        await assertError(
+            async () =>
+                await rld(expressionConfig, invalidOpMetas.invalid_bytes, false),
+            "invalid op meta",
+            "Invalid Error"
+        );
+    });
+
     it("should decompile a valid expressionConfig", async () => {
 
         const expression = rainlang`_: add(0x0a 0x14);`;
-
         const expressionConfig = await rlc(expression, opMeta);
-
-        const decompiledText = await (await rld(expressionConfig, opMeta, false)).getTextDocument()
+        const decompiledText = (await rld(expressionConfig, opMeta, false)).getTextDocument()
             .getText();
-
         assert(decompiledText === expression, `Expected: ${expression}\nActual ${decompiledText}`);
 
     });
 
-    it("should decompile a valid expressionConfig", async () => {
+    it("should decompile an expression referencing top stack items", async () => {
 
         const expression = rainlang`a: 10, b: 20, c: b;`;
-
         const expressionConfig = await rlc(expression, opMeta);
-
-        const decompiledText = await (await rld(expressionConfig, opMeta, false)).getTextDocument()
+        const decompiledText = (await rld(expressionConfig, opMeta, false)).getTextDocument()
             .getText();
         const expectedText = `_ _ _: 0x0a 0x14 read-memory<1 1>();`;
         assert(decompiledText === expectedText, `\nExpected: ${expectedText}\nActual: ${decompiledText}`);
-
     });
 
+    it("should fail if an opcode is not found in opmeta", async () => {
+        // Contains an invalid opcode with enum 1337
+        const expressionConfig: ExpressionConfig = {
+            sources: [hexlify([0, 13, 0, 3, 0, 77, 0, 144, 5, 57, 0, 0])],
+            constants: [10, 20]
+        };
+        await assertError(
+            async () =>
+                await rld(expressionConfig, opMeta, false),
+            "opcode with enum \\\"1337\\\" does not exist on OpMeta",
+            "Invalid Error"
+        );
+    });
 
 });
