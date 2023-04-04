c1ccd54db174125acbfba7dbe28c9d79ae6cc643
rouzwelt <rouzweltt@gmail.com>
2023-04-04 23:33:29 +0000

    add polygon subgraph url

--- src/parser/onChainOpMeta.ts
+++ src/parser/onChainOpMeta.ts
@@ -5,9 +5,13 @@ import { decodeRainMetaDocument, hexlify, MAGIC_NUMBERS } from "../utils";
  * @public Subgraph endpoints and their chain ids as key/value pairs
  */
 export const sgBook: { [chainId: number | string]: string } = {
-    "0x80001": "https://api.thegraph.com/subgraphs/name/rainprotocol/interpreter-registry",
-    0x80001: "https://api.thegraph.com/subgraphs/name/rainprotocol/interpreter-registry",
-    mumbai: "https://api.thegraph.com/subgraphs/name/rainprotocol/interpreter-registry",
+    "0x80001":      "https://api.thegraph.com/subgraphs/name/rainprotocol/interpreter-registry",
+    0x80001:        "https://api.thegraph.com/subgraphs/name/rainprotocol/interpreter-registry",
+    mumbai:         "https://api.thegraph.com/subgraphs/name/rainprotocol/interpreter-registry",
+    maticmum:       "https://api.thegraph.com/subgraphs/name/rainprotocol/interpreter-registry",
+    137:            "https://api.thegraph.com/subgraphs/name/rainprotocol/interpreter-registry-polygon",
+    polygon:        "https://api.thegraph.com/subgraphs/name/rainprotocol/interpreter-registry-polygon",
+    matic:          "https://api.thegraph.com/subgraphs/name/rainprotocol/interpreter-registry-polygon"
 };
 
 /**
