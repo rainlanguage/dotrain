d8b086c84b04c6c2758485904736e3657c2e7998
SocioDroid <sociodroid2019@gmail.com>
2023-04-11 23:32:47 +0530

    Added more failing tests [skip publish]

--- test/compiler/rainlangDecompiler.test.ts
+++ test/compiler/rainlangDecompiler.test.ts
@@ -5,7 +5,7 @@ import { rld } from "../../src/compiler/rainDecompiler";
 import { ExpressionConfig, getOpMetaFromSg } from "../../src";
 import { invalidOpMetas } from "../fixtures/opmeta";
 import { hexlify } from "ethers/lib/utils";
-import assert from "assert";
+import assert, { AssertionError } from "assert";
 
 async function testRainlangDecompiler(
     expression: string, expectedExpression: string, opMeta: string | Uint8Array
@@ -15,15 +15,19 @@ async function testRainlangDecompiler(
         .getText();
     // Passing decompiled text to compiler again to check for errors
     try {
-        await rlc(decompiledText, opMeta);
+        const recompiledExpression = await rlc(decompiledText, opMeta);
+        assert.deepEqual(recompiledExpression.sources, expressionConfig.sources);
     }
     catch (e) {
-        throw new Error(`Failed to compile the decompiled text:\n\ndecompiledText = ${decompiledText}\n\nError: ${JSON.stringify(e, null, 2)}`);
+        if (e instanceof AssertionError)
+            throw new Error(JSON.stringify(e, null, 2));
+        else
+            throw new Error(`Failed to compile the decompiled text:\n\ndecompiledText = ${decompiledText}\n\nError: ${JSON.stringify(e, null, 2)}`);
     }
     assert.equal(decompiledText, expectedExpression, `\nExpected: ${expectedExpression}\nActual: ${decompiledText}`);
 }
 
-describe.only("Rainlang Decompiler (rld) tests", async function () {
+describe("Rainlang Decompiler (rld) tests", async function () {
     let opMeta: string;
 
     before(async () => {
@@ -72,12 +76,88 @@ describe.only("Rainlang Decompiler (rld) tests", async function () {
 
     });
 
+    it("should decompile an expression with do-while opcode having multiple outputs", async () => {
+        const expression0 = rainlang`
+            c0: 1,
+            c1: 2,
+            condition: 1, 
+            _ _: do-while<1>(c0 c1 condition);
+            
+            s0 s1: ,
+            o0 o1: 1 2,
+            condition: 3; 
+    
+            s0: ,
+            _: less-than(s0 3);
+    
+            s0 s1: ,
+            _: add(s0 4),
+            _: add(s1 5);
+        `;
+        const expectedExpression0 = rainlang`_ _ _ _ _: 0x01 0x02 0x01 do-while<1>(read-memory<0 0>() read-memory<1 0>() read-memory<2 0>());
+        _ _ _: 0x01 0x02 0x03;
+        _: less-than(read-memory<0 0>() 0x03);
+        _ _: add(read-memory<0 0>() 0x04) add(read-memory<1 0>() 0x05);`;
+        await testRainlangDecompiler(expression0, expectedExpression0, opMeta);
+    });
+
+    it("should decompile an expression with loop-n opcode having multiple outputs", async () => {
+        const expression0 = rainlang`
+        _ loopoutput _: loop-n<1 1 3>(
+            2
+            3
+            4
+        ),
+        _ _ _ _ _ _ _ _: explode-32(loopoutput);
+       
+        s0 s1 s2: ,
+        increment: add(s0 5),
+
+        shrval: call<3 1>(increment s1 s2),
+        
+        lvldcr: saturating-sub(s2 1);
+        
+        s0 s1 s2: ,
+        levelmul: mul(6 s2),
+        levelexp: exp(2 levelmul),
+        finalmul: mul(levelexp s0),
+
+        op: add(finalmul s1);
+        `;
+        const expectedExpression0 = rainlang`_ _ _ _ _ _ _ _ _ _ _: loop-n<1 1 3>(0x02 0x03 0x04) explode-32(read-memory<1 0>());
+        _ _ _: add(read-memory<0 0>() 0x05) call<3 1>(read-memory<3 0>() read-memory<1 0>() read-memory<2 0>()) saturating-sub(read-memory<2 0>() 0x01);
+        _ _ _ _: mul(0x06 read-memory<2 0>()) exp(0x02 read-memory<3 0>()) mul(read-memory<4 0>() read-memory<0 0>()) add(read-memory<5 0>() read-memory<1 0>());`;
+        await testRainlangDecompiler(expression0, expectedExpression0, opMeta);
+    });
+
     it("should decompile an expression with call opcode having multiple outputs", async () => {
         const expression0 = rainlang`
             /* main source */
             _ _ _:  call<1 3>(2 2);
         `;
-        const expectedExpression0 = rainlang` _ _ _: call<1 3>(0x02 0x02);`;
+        const expectedExpression0 = rainlang`_ _ _: call<1 3>(0x02 0x02);`;
+        await testRainlangDecompiler(expression0, expectedExpression0, opMeta);
+    });
+
+    it("should decompile an expression with an opcode having multiple outputs", async () => {
+        const expression0 = rainlang`        
+            _ _: erc-1155-balance-of-batch(
+                0x01
+                0x02
+                0x03
+                2
+                3
+            );
+        `;
+        const expectedExpression0 = rainlang`_ _: erc-1155-balance-of-batch(0x01 0x02 0x03 0x02 0x03);`;
+        await testRainlangDecompiler(expression0, expectedExpression0, opMeta);
+    });
+
+    it("should decompile an expression with fold-context opcode having multiple outputs", async () => {
+        const expression0 = rainlang`        
+           _ _: fold-context<2 3 1>(0 0);
+        `;
+        const expectedExpression0 = rainlang`_ _: fold-context<2 3 1>(0x00 0x00);`;
         await testRainlangDecompiler(expression0, expectedExpression0, opMeta);
     });
 
