d7fa58a5a12f6aabaeb8846f59b6d42881351bbe
rouzwelt <rouzweltt@gmail.com>
2024-01-05 00:29:55 +0000

    update flakes

--- flake.nix
+++ flake.nix
@@ -45,6 +45,21 @@
         buildInputs = [
           rust
         ] ++ (with pkgs; [ 
+          gmp
+          iconv 
+          openssl 
+          pkg-config
+        ]) ++ (pkgs.lib.optionals pkgs.stdenv.isDarwin [
+          pkgs.libiconv
+          pkgs.darwin.apple_sdk.frameworks.Security
+          pkgs.darwin.apple_sdk.frameworks.CoreServices
+          pkgs.darwin.apple_sdk.frameworks.CoreFoundation
+          pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
+        ]);
+        nativeBuildInputs = [
+          rust
+        ] ++ (with pkgs; [ 
+          gmp
           iconv 
           openssl 
           pkg-config
@@ -71,21 +86,23 @@
           emscripten
           nodejs-18_x
           wasm-bindgen-cli
-        ]) ++ (with node-cmd; [
-          build
-          hard-build
-          build-wasm
-          build-bindings
-          node-bg
-          web-bg
-          local-test
-          ci-test
-          flush
-          hard-flush
-          docgen
-          lint
-          lint-fix
-          lint-bindings
+          (writeShellScriptBin "flush" ''
+            rm -rf dist
+            rm -rf docs
+            rm -rf temp
+          '')
+          (writeShellScriptBin "hard-flush" ''
+            rm -rf dist
+            rm -rf docs
+            rm -rf temp
+            rm -rf target
+            rm -rf node_modules
+          '')
+          (writeShellScriptBin "hard-build" ''
+            hard-flush
+            npm install
+            npm run build
+          '')
         ]) ++ (pkgs.lib.optionals pkgs.stdenv.isDarwin [
           pkgs.libiconv
           pkgs.darwin.apple_sdk.frameworks.Security
@@ -94,76 +111,6 @@
           pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
         ]);
         shellHook = '' npm install '';
-      };
-
-      # dev shell npm commands
-      node-cmd = rec {
-        local-test = pkgs.writeShellScriptBin "local-test" ''
-          npm test
-        '';
-
-        flush = pkgs.writeShellScriptBin "flush" ''
-          rm -rf dist
-          rm -rf docs
-          rm -rf temp
-        '';
-
-        hard-flush = pkgs.writeShellScriptBin "hard-flush" ''
-          rm -rf dist
-          rm -rf docs
-          rm -rf temp
-          rm -rf target
-          rm -rf node_modules
-        '';
-
-        ci-test = pkgs.writeShellScriptBin "ci-test" ''
-          ${hard-flush}/bin/hard-flush
-          npm install
-          npm run build
-          npm test
-        '';
-
-        build-wasm = pkgs.writeShellScriptBin "build-wasm" ''
-          npm run build-wasm
-        '';
-
-        node-bg = pkgs.writeShellScriptBin "node-bg" ''
-          npm run node-bg
-        '';
-
-        web-bg = pkgs.writeShellScriptBin "web-bg" ''
-          npm run web-bg
-        '';
-
-        build-bindings = pkgs.writeShellScriptBin "build-bindings" ''
-          npm run build-bindings
-        '';
-
-        build = pkgs.writeShellScriptBin "build" ''
-          npm run build
-        '';
-
-        hard-build = pkgs.writeShellScriptBin "hard-build" ''
-          ${hard-flush}/bin/hard-flush
-          npm install
-          npm run build
-        '';
-
-        docgen = pkgs.writeShellScriptBin "docgen" ''
-          npm run docgen
-        '';
-
-        lint = pkgs.writeShellScriptBin "lint" ''
-          npm run lint
-        '';
-
-        lint-fix = pkgs.writeShellScriptBin "lint-fix" ''
-          npm run lint-fix
-        '';
-
-        lint-bindings = pkgs.writeShellScriptBin "lint-bindings" ''
-          npm run lint-bindings
-        '';
       };
     }
   );
