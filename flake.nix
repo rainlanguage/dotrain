19330af0ba21e92f85e1f41cedac9c92e5dced84
rouzwelt <rouzweltt@gmail.com>
2024-01-05 19:21:12 +0000

    update

--- flake.nix
+++ flake.nix
@@ -42,15 +42,13 @@
           mkdir -p $out/bin
           cp target/release/dotrain $out/bin/
         '';
-        # buildInputs = with pkgs; [ 
-        #   openssl 
-        #   pkg-config
-        # ];
         buildInputs = with pkgs; [ 
-          # gmp
-          # iconv
-          openssl 
+          # openssl 
           pkg-config
+        ];
+        nativeBuildInputs = with pkgs; [ 
+          openssl 
+          # pkg-config
         ] ++ lib.optionals stdenv.isDarwin [
           darwin.apple_sdk.frameworks.SystemConfiguration
         ];
