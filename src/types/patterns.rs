d692b58c41f2167c88f846215cfb052f8d4bd394
rouzwelt <rouzweltt@gmail.com>
2024-01-12 19:21:16 +0000

    fmt

--- src/types/patterns.rs
+++ src/types/patterns.rs
@@ -111,15 +111,18 @@ mod tests {
         }
         // invalids
         for i in ["♥", "∴"] {
-            assert!(
-                ILLEGAL_CHAR.is_match(i),
-                "String '{}' considered valid.",
-                i
-            );
+            assert!(ILLEGAL_CHAR.is_match(i), "String '{}' considered valid.", i);
         }
 
         // invalids
-        for i in ["-abcd", "-abcd-efg", "1abcd-efg", "1234", "_abcd-efg", "AkjhJ-Qer"] {
+        for i in [
+            "-abcd",
+            "-abcd-efg",
+            "1abcd-efg",
+            "1234",
+            "_abcd-efg",
+            "AkjhJ-Qer",
+        ] {
             assert!(
                 !WORD_PATTERN.is_match(i),
                 "String '{}' considered valid.",
@@ -136,7 +139,12 @@ mod tests {
         }
 
         // invalids
-        for i in ["0x1234abcd", "x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef", "0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdefa", "0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdeg"] {
+        for i in [
+            "0x1234abcd",
+            "x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef",
+            "0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdefa",
+            "0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdeg",
+        ] {
             assert!(
                 !HASH_PATTERN.is_match(i),
                 "String '{}' considered valid.",
@@ -144,7 +152,10 @@ mod tests {
             );
         }
         // valids
-        for i in ["0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef", "0x0000000000000000000000000000000000000000000000000000000000000000"] {
+        for i in [
+            "0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef",
+            "0x0000000000000000000000000000000000000000000000000000000000000000",
+        ] {
             assert!(
                 HASH_PATTERN.is_match(i),
                 "String '{}' considered invalid.",
@@ -154,11 +165,7 @@ mod tests {
 
         // invalids
         for i in ["x123abcd", "123", "0b101", "4e15"] {
-            assert!(
-                !HEX_PATTERN.is_match(i),
-                "String '{}' considered valid.",
-                i
-            );
+            assert!(!HEX_PATTERN.is_match(i), "String '{}' considered valid.", i);
         }
         // valids
         for i in ["0x123abcd", "0x1234567890abcdef", "0x123AbcDeF"] {
@@ -188,11 +195,7 @@ mod tests {
 
         // invalids
         for i in ["0x123abcdef", "0b10101", "4e15"] {
-            assert!(
-                !INT_PATTERN.is_match(i),
-                "String '{}' considered valid.",
-                i
-            );
+            assert!(!INT_PATTERN.is_match(i), "String '{}' considered valid.", i);
         }
         // valids
         for i in ["123", "1234567890", "83276401"] {
@@ -205,23 +208,21 @@ mod tests {
 
         // invalids
         for i in ["1235657", "0x1234abcdf", "0b101110", "e18"] {
-            assert!(
-                !E_PATTERN.is_match(i),
-                "String '{}' considered valid.",
-                i
-            );
+            assert!(!E_PATTERN.is_match(i), "String '{}' considered valid.", i);
         }
         // valids
         for i in ["3e16", "2345e12987234", "101e1001"] {
-            assert!(
-                E_PATTERN.is_match(i),
-                "String '{}' considered invalid.",
-                i
-            );
+            assert!(E_PATTERN.is_match(i), "String '{}' considered invalid.", i);
         }
 
         // invalids
-        for i in ["_abcd-efg.hij10-", ".1abcd-jh.", "abcd_er", "123abcd.ert-ety", "qSE.asd98.E87-weR"] {
+        for i in [
+            "_abcd-efg.hij10-",
+            ".1abcd-jh.",
+            "abcd_er",
+            "123abcd.ert-ety",
+            "qSE.asd98.E87-weR",
+        ] {
             assert!(
                 !NAMESPACE_PATTERN.is_match(i),
                 "String '{}' considered valid.",
@@ -229,7 +230,11 @@ mod tests {
             );
         }
         // valids
-        for i in ["abcd-efg", "abcd-efg.qwe2-wer-.kjh8.poi345.oiu", "a-12-asd.lkj8"] {
+        for i in [
+            "abcd-efg",
+            "abcd-efg.qwe2-wer-.kjh8.poi345.oiu",
+            "a-12-asd.lkj8",
+        ] {
             assert!(
                 NAMESPACE_PATTERN.is_match(i),
                 "String '{}' considered invalid.",
@@ -238,7 +243,12 @@ mod tests {
         }
 
         // invalids
-        for i in ["/ sjdkhfsfsdf /", "// asjkhdf", "/// kjhfdgd ", "\\* jkhjgk */"] {
+        for i in [
+            "/ sjdkhfsfsdf /",
+            "// asjkhdf",
+            "/// kjhfdgd ",
+            "\\* jkhjgk */",
+        ] {
             assert!(
                 !COMMENT_PATTERN.is_match(i),
                 "String '{}' considered valid.",
@@ -246,7 +256,11 @@ mod tests {
             );
         }
         // valids
-        for i in ["/* jhggf jhgj jkgkkytees54368 867 */", "/** kjhgkj */", "//** hjgjh 453sdratf jkghj */ khkj hjfghf"] {
+        for i in [
+            "/* jhggf jhgj jkgkkytees54368 867 */",
+            "/** kjhgkj */",
+            "//** hjgjh 453sdratf jkghj */ khkj hjfghf",
+        ] {
             assert!(
                 COMMENT_PATTERN.is_match(i),
                 "String '{}' considered invalid.",
@@ -256,28 +270,16 @@ mod tests {
 
         // invalids
         for i in ["k", "765", "/*&^", "-)("] {
-            assert!(
-                !WS_PATTERN.is_match(i),
-                "String '{}' considered valid.",
-                i
-            );
+            assert!(!WS_PATTERN.is_match(i), "String '{}' considered valid.", i);
         }
         // valids
         for i in [" ", "\n", "\t", "\r", " \n \t"] {
-            assert!(
-                WS_PATTERN.is_match(i),
-                "String '{}' considered invalid.",
-                i
-            );
+            assert!(WS_PATTERN.is_match(i), "String '{}' considered invalid.", i);
         }
 
         // valids
         for i in ["abcd.iuy1-hg", "123jhg-jkkj", "'Ajh", "qwS.oiE34-qwe.qw"] {
-            assert!(
-                !DEP_PATTERN.is_match(i),
-                "String '{}' considered valid.",
-                i
-            );
+            assert!(!DEP_PATTERN.is_match(i), "String '{}' considered valid.", i);
         }
         // valids
         for i in ["'abcd123", "'a123.kjh-iuy.oiu12-oiu2-.mnm"] {
@@ -297,7 +299,12 @@ mod tests {
             );
         }
         // valids
-        for i in ["123456789", "0x123abcdefAdfe", "'abcd12-jh2.oiu.lkj89-", "'.asd12-wer.jh45-iu78.lk9"] {
+        for i in [
+            "123456789",
+            "0x123abcdefAdfe",
+            "'abcd12-jh2.oiu.lkj89-",
+            "'.asd12-wer.jh45-iu78.lk9",
+        ] {
             assert!(
                 OPERAND_ARG_PATTERN.is_match(i),
                 "String '{}' considered invalid.",
@@ -307,11 +314,7 @@ mod tests {
 
         // invalids
         for i in ["-", "AbkE12", "12kjh", "-abcd-iuy", "sad12.jh5"] {
-            assert!(
-                !LHS_PATTERN.is_match(i),
-                "String '{}' considered valid.",
-                i
-            );
+            assert!(!LHS_PATTERN.is_match(i), "String '{}' considered valid.", i);
         }
         // valids
         for i in ["abced67", "_", "as12-iuy-", "a12-456-678-"] {
