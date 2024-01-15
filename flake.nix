1302ae01ba916b3830ab51e0f8b3a2561df87b15
thedavidmeister <thedavidmeister@gmail.com>
2024-01-15 08:57:57 +0400

    wip on rainix bump

--- flake.nix
+++ flake.nix
@@ -8,34 +8,39 @@
 
   flake-utils.lib.eachDefaultSystem (system:
     let
+      pkgs = rainix.pkgs.${system};
     in rec {
+      packages = {
+        build-bin = (pkgs.makeRustPlatform{
+          rustc = rainix.rust-toolchain.${system};
+          cargo = rainix.rust-toolchain.${system};
+        }).buildRustPackage {
+          src = ./.;
+          doCheck = false;
+          name = "dotrain";
+          cargoLock.lockFile = ./Cargo.lock;
+          # allows for git deps to be resolved without the need to specify their outputHash
+          cargoLock.allowBuiltinFetchGit = true;
+          buildPhase = ''
+            cargo build --release --bin dotrain --features cli
+          '';
+          installPhase = ''
+            mkdir -p $out/bin
+            cp target/release/dotrain $out/bin/
+          '';
+          buildInputs = with pkgs; [
+            openssl
+          ];
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+          ] ++ lib.optionals stdenv.isDarwin [
+            darwin.apple_sdk.frameworks.SystemConfiguration
+          ];
+        };
+      } // rainix.packages.${system};
+
       # # For `nix build` & `nix run`:
-      defaultPackage = (rainix.pkgs.makeRustPlatform{
-        rustc = rainix.rust-toolchain;
-        cargo = rainix.rust-toolchain;
-      }).buildRustPackage {
-        src = ./.;
-        doCheck = false;
-        name = "dotrain";
-        cargoLock.lockFile = ./Cargo.lock;
-        # allows for git deps to be resolved without the need to specify their outputHash
-        cargoLock.allowBuiltinFetchGit = true;
-        buildPhase = ''
-          cargo build --release --bin dotrain --features cli
-        '';
-        installPhase = ''
-          mkdir -p $out/bin
-          cp target/release/dotrain $out/bin/
-        '';
-        buildInputs = with rainix.pkgs; [
-          openssl
-        ];
-        nativeBuildInputs = with rainix.pkgs; [
-          pkg-config
-        ] ++ lib.optionals stdenv.isDarwin [
-          darwin.apple_sdk.frameworks.SystemConfiguration
-        ];
-      };
+      defaultPackage = packages.build-bin;
 
       # For `nix develop`:
       devShells = rainix.devShells.${system};
