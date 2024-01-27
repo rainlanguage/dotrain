5b710b05698b37452a9f7011bd0344520c9d8c1e
rouzwelt <rouzweltt@gmail.com>
2024-01-27 22:14:42 +0000

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,4 @@
-pub mod cli;
-pub mod types;
-pub mod error;
-pub mod parser;
-pub mod composer;
-
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> anyhow::Result<()> {
-    cli::main().await
+    dotrain::cli::main().await
 }
