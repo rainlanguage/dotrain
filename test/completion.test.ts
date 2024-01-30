7d0fdcdf4478478c2dfcc70a34345410ce004567
rouzwelt <rouzweltt@gmail.com>
2024-01-30 17:36:39 +0000

    add tests

---
+++ test/completion.test.ts
@@ -0,0 +1,163 @@
+import * as assert from "assert";
+import { rainlang, MetaStore, RainDocument, RainLanguageServices } from "../dist/cjs";
+import {
+    Position,
+    CompletionItem,
+    TextDocumentItem,
+    CompletionItemKind,
+} from "vscode-languageserver-types";
+
+const deployerHash = "                                                                   ";
+async function testCompletion(
+    text: string,
+    position: Position,
+    expectedCompletions: CompletionItem[] | null,
+    services: RainLanguageServices,
+) {
+    const actualCompletions = services.doComplete(
+        TextDocumentItem.create("file:///completion.test.rain", "rainlang", 1, text),
+        position,
+    );
+    if (expectedCompletions === null) assert.ok(actualCompletions === null);
+    else {
+        assert.ok(actualCompletions?.length == expectedCompletions?.length);
+        expectedCompletions.forEach((item, i) => {
+            assert.equal(actualCompletions[i].label, item.label);
+            assert.equal(actualCompletions[i].kind, item.kind);
+        });
+    }
+}
+
+describe("LSP Code Completion Language Service Tests", async function () {
+    const store = new MetaStore();
+    const AllOpcodeCompletions: CompletionItem[] = [];
+
+    const services = new RainLanguageServices(store);
+
+    it("should provide all opcode suggestions for rhs when there is no lhs aliases", async () => {
+        const _allCompletions = [...AllOpcodeCompletions];
+        _allCompletions.unshift({
+            label: "expression",
+            kind: CompletionItemKind.Class,
+        });
+        await testCompletion(
+            rainlang`${deployerHash} #expression _: `,
+            Position.create(0, 82),
+            _allCompletions,
+            services,
+        );
+    });
+
+    it("should provide correct suggestions based on trailing characters", async () => {
+        const _allCompletions = [...AllOpcodeCompletions];
+        _allCompletions.unshift({
+            label: "exp",
+            kind: CompletionItemKind.Class,
+        });
+        await testCompletion(
+            rainlang`${deployerHash} #exp _: int-ad`,
+            Position.create(0, 82),
+            _allCompletions,
+            services,
+        );
+    });
+
+    it("should provide no suggestions if cursor is in middle of a word", async () => {
+        await testCompletion(
+            rainlang`${deployerHash} #exp _: add`,
+            Position.create(0, 77),
+            null,
+            services,
+        );
+    });
+
+    it("should provide correct suggestions if leading character is non-word", async () => {
+        const _allCompletions = [...AllOpcodeCompletions];
+        _allCompletions.unshift({
+            label: "exp",
+            kind: CompletionItemKind.Class,
+        });
+        await testCompletion(
+            rainlang`${deployerHash} #exp _: add(1 2)`,
+            Position.create(0, 79),
+            _allCompletions,
+            services,
+        );
+    });
+
+    it("should include lhs alias in suggestions", async () => {
+        const _allCompletions = [...AllOpcodeCompletions];
+        await testCompletion(
+            rainlang`${deployerHash} #exp name: n`,
+            Position.create(0, 80),
+            [
+                // {
+                //     label: "name",
+                //     kind: CompletionItemKind.Variable,
+                // },
+                {
+                    label: "exp",
+                    kind: CompletionItemKind.Class,
+                },
+                ..._allCompletions,
+            ],
+            services,
+        );
+    });
+
+    it("should include root namespaces items in suggestions", async () => {
+        const _expression = rainlang`${deployerHash}
+#row
+1
+
+#main
+_: .`;
+        const _dotrain = RainDocument.create(_expression, store);
+        const _ns = _dotrain.namespace;
+        const items: CompletionItem[] = [];
+        _ns.forEach((v, k) => {
+            items.push({
+                label: k,
+                kind: !("element" in v)
+                    ? CompletionItemKind.Field
+                    : "content" in v.element
+                      ? CompletionItemKind.Class
+                      : CompletionItemKind.Function,
+            });
+        });
+        await testCompletion(
+            _expression,
+            Position.create(5, 4),
+            items.filter((v) => v.label !== "Dispair"),
+            services,
+        );
+    });
+
+    it("should include correct namespaces items in suggestions", async () => {
+        const _expression = rainlang`${deployerHash}
+#row
+1
+
+#main
+_: .r`;
+        const _dotrain = RainDocument.create(_expression, store);
+        const _ns = _dotrain.namespace;
+        const items: CompletionItem[] = [];
+        _ns.forEach((v, k) => {
+            items.unshift({
+                label: k,
+                kind: !("element" in v)
+                    ? CompletionItemKind.Field
+                    : "content" in v.element
+                      ? CompletionItemKind.Class
+                      : CompletionItemKind.Function,
+            });
+        });
+        await testCompletion(
+            _expression,
+            Position.create(5, 5),
+            items.filter((v) => v.label !== "Dispair"),
+            services,
+        );
+    });
+});
---
+++ test/utils.ts
@@ -0,0 +1,40 @@
+import { assert } from "chai";
+import { Position, Range } from "vscode-languageserver-types";
+
+export const testDotrain = `#value 0x123
+
+#other-value 0b101
+
+#my-address 0x1234567890123456789012345678901234567890
+
+#elided ! this is elided, rebind before using`;
+
+export const testDotrainUri = "file:///test.rain";
+
+/**
+ * Assert errors thrown by functions
+ */
+export const assertError = async (f: any, s: string, e: string) => {
+    let didError = false;
+    try {
+        await f();
+    } catch (e: any) {
+        assert(
+            JSON.stringify(e).includes(s),
+            `error string ${JSON.stringify(e)} does not include ${s}`,
+        );
+        didError = true;
+    }
+    assert(didError, e);
+};
+
+/**
+ * Creates Range
+ * @param sLine Starting line number
+ * @param sChar Starting character position
+ * @param eLine Ending line number
+ * @param eChar Ending character position
+ */
+export function toRange(sLine: number, sChar: number, eLine: number, eChar: number) {
+    return Range.create(Position.create(sLine, sChar), Position.create(eLine, eChar));
+}
